cdd307f412f9455ace6a2fbff633e20d7742662d
eugsalazar <109601517+eugsalazar@users.noreply.github.com>
2022-07-20 11:29:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Ricket Smoothie')
+streamlit.text('hard-Boiled Free-Range Egg')
