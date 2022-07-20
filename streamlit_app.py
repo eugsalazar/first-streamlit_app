25282e203a6fff10d862a79989041ccc616c136f
eugsalazar <109601517+eugsalazar@users.noreply.github.com>
2022-07-20 13:10:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,4 @@ streamlit.text('🐔 hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
