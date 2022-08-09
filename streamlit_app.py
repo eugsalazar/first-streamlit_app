f12f233f82d9d1cf72a86058d7a2f86bed1ff23c
eugsalazar <109601517+eugsalazar@users.noreply.github.com>
2022-08-09 16:20:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
+my_cur = my_cnx.cursor()
 #add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
 #my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST where FRUIT_NAME = '"+ add_my_fruit + "'" )
 #streamlit.write('The user entered ', add_my_fruit)
